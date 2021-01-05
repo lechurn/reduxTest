2aeb3a46a7397d3a83592dbb66a712737db055e6
Lee Churn (NAX) <leechurn.nax@gmail.com>
2021-01-05 10:21:30 +0800

    1st commit for ReactJS

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducer from './reducers';
+import {Provider} from 'react-redux';
+
+const store = createStore(
+    allReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+  );
+
+
+// import {createStore} from 'redux';
+
+// //STORE -> GLOBALIZED STATE
+
+// //ACTION INCREMENT
+// const increment = () => {
+//   return {
+//     type:'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type:'DECREMENT'
+//   }
+// }
+
+// //REDUCER
+// const counter = (state = 0 , action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//         return state - 1;
+
+//   }
+// }
+
+// let store = createStore(counter);
+
+// //Display it in the console
+// store.subscribe(()=> console.log(store.getState()));
+
+// //DISPATCH
+// store.dispatch(increment());
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
